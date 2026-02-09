c5a7a2f471c9383239f22c03110e0c72b0e9f669
withskill-source <withskill@gmail.com>
2026-02-08 22:59:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/api/games', (req, res) => {
         {
             id: 6,
             name: 'Pac-Man',
-            url: 'https://pacman.com/en/',
+            url: 'https://www.google.com/logos/2010/pacman10-i.html',
             thumbnail: 'PM',
             color: '#f1c40f',
             description: 'Classic arcade maze game',
